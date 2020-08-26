9cf5efb266c762e51e535e36e2d8434db5888905
Luiz Yao <yaomeng614@gmail.com>
2020-08-26 09:02:45 +0800

    :bulb: fix comment express typos

--- src/pytest_pyppeteer/hooks.py
+++ src/pytest_pyppeteer/hooks.py
@@ -2,7 +2,7 @@ from _pytest.nodes import Item
 
 
 async def pytest_pyppeteer_targets_setup(item: Item):
-    """Called to teardown target before execute a test item."""
+    """Called to setup target before execute a test item."""
 
 
 async def pytest_pyppeteer_targets_teardown(item: Item):
