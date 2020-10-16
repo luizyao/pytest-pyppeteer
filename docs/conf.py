4f704760210f285679a701fd1923cca7ca5674d4
Yao Meng <yaomeng614@gmail.com>
2020-10-16 15:11:37 +0800

    :hammer: use githubpages sphinx extension

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ extensions = [
     "sphinx.ext.todo",
     "sphinx.ext.viewcode",
     "sphinx.ext.intersphinx",
+    "sphinx.ext.githubpages",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -77,4 +78,4 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
+html_static_path = ["_static"]
